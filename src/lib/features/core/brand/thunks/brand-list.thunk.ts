c7431f453b5fd7b1842bb91b9dd0597c2f80c514
Carlos Sosa <carloseduardososagonzalez@gmail.com>
2026-02-02 13:29:51 -0600

    feat: fix API baseURL and add logging to brand list thunk

--- src/lib/features/core/brand/thunks/brand-list.thunk.ts
+++ src/lib/features/core/brand/thunks/brand-list.thunk.ts
@@ -5,7 +5,14 @@ import api from "@/src/lib/axios";
 export const getBrandList = createAsyncThunk(
   "brands/list",
   async (): Promise<BrandListResponse> => {
-    const response = await api.get<BrandListResponse>(`/brand/list`);
-    return response.data;
+    console.log("antes de hacer el llamado a la API...");
+    try {
+      const response = await api.get<BrandListResponse>(`/brand/list`);
+      console.log("Brand List Response:", response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error en Brand List:", error);
+      throw error;
+    }
   },
 );
